838eb9492684e58e3e66aed7724ce93285fdaf93
ritullia <brigrita@gmail.com>
2023-02-16 17:10:39 +0200

    type correction in index.html

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos
 -------------------------------------------------------------------------------------------- */
 
+
 let a = composition(one, four);
 let b = division(four, two);
 let c = substraction(three, two);
